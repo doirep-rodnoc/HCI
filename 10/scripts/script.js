9ab0853af46d6d290c202b072cdabaeaa3c7973d
doirep_rodnoc <51051070+doirep-rodnoc@users.noreply.github.com>
2022-12-08 05:51:10 +0900

    20221208-0551

--- 10/scripts/script.js
+++ 10/scripts/script.js
@@ -1,13 +1,12 @@
-
 const username = "username";
 const pwd = "HCI";
-
 
 const errorSound = new Audio('sounds/fail.mp3');
 const startSound = new Audio('sounds/start.mp3');
 const shakuhachiSound = new Audio('sounds/shakuhachi.mp3');
 const suiryuSound = new Audio('sounds/suiryu.mp3');
 const suimenSound = new Audio('sounds/suimen.mp3');
+
 var fishmove = false;
 var bubbleVel = 0.5;
 var fishForm = document.getElementsByClassName('fishForm')[0];
@@ -18,9 +17,14 @@ var colliderRect;
 console.log("hello");
 
 window.addEventListener('load', function(){
-  gsap.to('.foreground', {duration: 1, y: '-100%'});
+  gsap.to('.foreground', {duration: 1, y: '-100%'}); //ページ読み込み時の白前景を上に
 });
 
+/**
+ * bubbly-bg.jsを用いた背景のアニメーション．
+ * 引数で速度を指定することで泡の速度が変化する．
+ * @param {*} bubbleVel 
+ */
 function bubbleEffect(bubbleVel){
   bubbly({
     blur:5,
@@ -36,6 +40,7 @@ function bubbleEffect(bubbleVel){
 
 bubbleEffect(0.5);
 
+/**ページ読み込み完了時の処理 */
 window.addEventListener('load', function(){
   gsap.set(".fishForm", {x: 100, opacity: 0, rotate: 10}); // おさかな初期位置
   gsap.set("#caption", {x: 100, opacity: 0}); // 説明文初期位置
@@ -45,6 +50,7 @@ window.addEventListener('load', function(){
   fishingTools.style.left = (Math.random() * 70) + '%';
 });
   
+/** 入力フィールドに変化があった時に魚を振動させるアニメーション */
 document.querySelectorAll(".inputField").forEach((elm) => {
   elm.addEventListener('change', function(){
     gsap.to(".fishForm", 0.2, {
@@ -55,8 +61,9 @@ document.querySelectorAll(".inputField").forEach((elm) => {
   });
 });
 
+/** ログインボタンが押された時の処理 */
 document.getElementById('regBtn').addEventListener('click', function(){
-  if((document.getElementById('uName').value) && (document.getElementById('pwdField').value)){   
+  if((document.getElementById('uName').value) && (document.getElementById('pwdField').value)){ // 入力フィールドが両方空白でなければ 
     gsap.to(".regButtonField", {duration: 0.5, opacity: 0});
     gsap.to("#caption", {duration: 0.5, opacity: 0});
     gsap.to(".fishingTools", {duration: 1, y: 300});
@@ -73,7 +80,6 @@ document.getElementById('regBtn').addEventListener('click', function(){
     gsap.to("#tsurarero", {duration: 0.5, scale: 1, opacity: 100});
     gsap.to("#tsurarero", {delay: 3, duration: 0.1, opacity: 0});
 
-
     fishmove = true; 
     shakuhachiSound.currentTime = 0;
     shakuhachiSound.volume = 0.5;
@@ -82,7 +88,7 @@ document.getElementById('regBtn').addEventListener('click', function(){
     startSound.play();
     suimenSound.currentTime = 0;
     suimenSound.play();
-  }else{
+  }else{ // 入力フィールドに未入力がある場合の処理
     errorSound.currentTime = 0;
     errorSound.play();
     document.getElementById('errmsg').textContent = "入力されていない項目があります．";
@@ -94,11 +100,16 @@ document.getElementById('regBtn').addEventListener('click', function(){
   }
 });
 
+/** 
+ * 魚のマウスストーカの処理．マウスが動くたびに呼び出されるが，
+ * fishmoveフラグがfalseの間は魚は動かない．
+ */
 document.addEventListener('mousemove', function(e){
   if(fishmove){     
     fishForm.style.position = 'fixed'; 
     gsap.to(".fishForm", {duration: 0.3, x:(e.clientX - 10), y:(e.clientY - 510)});
 
+    /** マウスカーソルが指定の位置(釣り針の位置)に来たときの処理(釣られる処理) */
     if(((-40 < (colliderRect.x - e.clientX) && (colliderRect.x - e.clientX) < -10) && Math.abs(300-e.clientY) < 20)){
       suimenSound.currentTime = 0;
       suimenSound.play();
@@ -134,10 +145,12 @@ document.addEventListener('mousemove', function(e){
   }
 });
 
+//釣り針の当たり判定のウィンドウサイズ変更時に対応するための処理
 window.addEventListener('resize', function(){
   colliderRect = collider.getBoundingClientRect(); 
 });
 
+//ブラウザバックされた場合の再読み込み
 window.onpageshow = function(event) {
 	if (event.persisted) {
 		 window.location.reload();
